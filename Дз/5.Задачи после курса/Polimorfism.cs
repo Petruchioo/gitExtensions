b694076e3432a6dbba89f1359ba97d756d1cc288
Petruchioo <gagonader02@mail.ru>
2025-05-06 22:55:37 +0500

    +Add новые задачи

---
+++ Дз/5.Задачи после курса/Polimorfism.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO; //пространство имен
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Media;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.Design;
+
+namespace Обучение_.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            animals.Add(new Rabbit());
+            animals.Add(new Wolf());
+            animals.Add(new Fox());
+
+            foreach (var animal in animals)
+            {
+                animal.Run();
+                Console.WriteLine(new String('-', 15) + "выебываюсь" + new String('-', 15));
+            }
+        }
+
+        abstract class Animal
+        {
+            public virtual void Run()
+            {
+                Console.WriteLine("I am animal and I am running for my life!");
+            }
+        }
+
+        class Rabbit : Animal 
+        {
+            public override void Run()
+            {
+                Console.WriteLine("I am running from predators");
+            }
+        }
+        
+        class Wolf : Animal 
+        {
+            
+        }
+
+        class Fox : Animal
+        {
+            public override void Run()
+            {
+                Console.WriteLine(" I am running for tasty rabbit ;)");
+            }
+        }
+    }
+}
---
+++ Дз/5.Задачи после курса/interface.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO; //пространство имен
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Media;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.Design;
+
+namespace Обучение_.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var constantReader = new ConstantReader();
+            var consoleReader = new ConsoleReader();
+
+            //var constantParser = new IntegerParser(new ConstantReader());
+            //int constantResult = constantParser.Parse();
+
+            //var consoleParser = new IntegerParser(new ConsoleReader());
+            //int consoleResult = consoleParser.Parse();
+
+            var integerParser = new IntegerParser(constantReader).Parse();
+            var integerParser2 = new IntegerParser(consoleReader).Parse();
+        }
+
+        interface IInputReader
+        {
+            string ReaderFromSource();
+        }
+
+        class ConstantReader : IInputReader
+        {
+            public string ReaderFromSource()
+            {
+                return "5";
+            }
+        }
+
+        class ConsoleReader : IInputReader
+        {
+            public string ReaderFromSource()
+            {
+                return Console.ReadLine();
+            }
+        }
+
+        class IntegerParser
+        {
+            private IInputReader _reader;
+
+            public IntegerParser(IInputReader reader) //нейронка не до конца понимаю: создается констркутор котрый принимает интерфейс и переменную, переменная обьявляется, а дальше затуп
+            {
+                _reader = reader;
+            }
+
+            public int Parse()
+            {
+                var inputedValue = _reader.ReaderFromSource();
+                return int.Parse(inputedValue);
+            }
+        }
+    }
+}
